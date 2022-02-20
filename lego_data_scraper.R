7baa931d079a4300acace1bbe591f109c3f9aba1
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2022-02-20 20:37:33 +0200

    completed nested page attributes retrieval for age, theme data and set numbers

--- lego_data_scraper.R
+++ lego_data_scraper.R
@@ -8,18 +8,25 @@ page = 1
 url = glue::glue("https://brickset.com/sets/year-{current_year-1}/filter-Released/page-{page}")
 
 # obtain html document from url
-lego_data = read_html(url)
+legoData = read_html(url)
+
+# obtain links for nested pages
+setLinks = legoData %>% 
+  html_nodes("h1 a") %>%
+  html_attr("href") %>%
+  paste("https://brickset.com/", ., sep = "")
 
 # define table attributes; 
 # html nodes function is used to select parts of a document using CSS selectors
 # html text function is used to extract the text the selected nodes
-setName = lego_data %>%
-  html_nodes("div.highslide-caption > h1") %>%
-  html_text()
+setName = legoData %>%
+  html_nodes(xpath = ".//h1/a/text()") %>%
+  html_text() %>%
+  str_trim()
 
 # sapply function is used to keep the data consistent in case of missing values
 # the function checks if the html node "rating" exists and if not it adds and NA 
-rating = lego_data %>% 
+rating = legoData %>% 
   html_nodes(".meta")
 
 rating = sapply(rating, function(feature){feature %>% 
@@ -28,7 +35,7 @@ rating = sapply(rating, function(feature){feature %>%
     str_trim() %>% as.character()}) %>% 
   sapply(function(feature) ifelse(length(feature) == 0, NA, feature))
 
-features = lego_data %>% 
+features = legoData %>% 
   html_nodes(".rating+ .col")
 pieces = sapply(features,function(feature) {feature %>%
     html_nodes(xpath ="./dl/dt[text() = 'Pieces']//following-sibling::dd/a/text()") %>%
@@ -78,5 +85,97 @@ setType = sapply(features,function(feature) {feature %>%
     str_trim() %>% as.character()
 }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
 
-test = data.frame(setName, rating, pieces, minifigs, RRP, PPP, packaging, availability, instructions, setType)
+#nested pages attriute retrieval functions 
+getSetNumber = function(setLink){
+  setPage = read_html(setLink) 
+  details = setPage %>% 
+    html_nodes(xpath = "//body/div[@class='outerwrap']/div[@class='content']/aside[@role='complementary']/section[2]/div[1]")
+  
+  set_number = sapply(details,function(feature) {feature %>%
+      html_nodes(xpath ="./dl/dt[text() = 'Set number']//following-sibling::dd[1]") %>%
+      html_text() %>%
+      str_trim() %>% as.character()
+  }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
+  
+  return(set_number)
+}
+
+getThemeGroup = function(setLink){
+  setPage = read_html(setLink) 
+  details = setPage %>% 
+    html_nodes(xpath = "//body/div[@class='outerwrap']/div[@class='content']/aside[@role='complementary']/section[2]/div[1]")
+  
+  set_themeGroup = sapply(details,function(feature) {feature %>%
+      html_nodes(xpath ="./dl/dt[text() = 'Theme group']//following-sibling::dd[1]") %>%
+      html_text() %>%
+      str_trim() %>% as.character()
+  }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
+  
+  return(set_themeGroup)
+}
+
+getTheme = function(setLink){
+  setPage = read_html(setLink) 
+  details = setPage %>% 
+    html_nodes(xpath = "//body/div[@class='outerwrap']/div[@class='content']/aside[@role='complementary']/section[2]/div[1]")
+  
+  set_theme = sapply(details,function(feature) {feature %>%
+      html_nodes(xpath ="./dl/dt[text() = 'Theme']//following-sibling::dd[1]/a/text()") %>%
+      html_text() %>%
+      str_trim() %>% as.character()
+  }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
+  
+  return(set_theme)
+}
+
+getSubtheme = function(setLink){
+  setPage = read_html(setLink) 
+  details = setPage %>% 
+    html_nodes(xpath = "//body/div[@class='outerwrap']/div[@class='content']/aside[@role='complementary']/section[2]/div[1]")
+  
+  set_subtheme = sapply(details,function(feature) {feature %>%
+      html_nodes(xpath ="./dl/dt[text() = 'Subtheme']//following-sibling::dd[1]/a/text()") %>%
+      html_text() %>%
+      str_trim() %>% as.character()
+  }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
+  
+  return(set_subtheme)
+}
+
+getYear = function(setLink){
+  setPage = read_html(setLink) 
+  details = setPage %>% 
+    html_nodes(xpath = "//body/div[@class='outerwrap']/div[@class='content']/aside[@role='complementary']/section[2]/div[1]")
+  
+  set_year = sapply(details,function(feature) {feature %>%
+      html_nodes(xpath ="./dl/dt[text() = 'Year released']//following-sibling::dd[1]/a/text()") %>%
+      html_text() %>%
+      str_trim() %>% as.character()
+  }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
+  
+  return(set_year)
+}
+
+getAge = function(setLink){
+  setPage = read_html(setLink) 
+  details = setPage %>% 
+    html_nodes(xpath = "//body/div[@class='outerwrap']/div[@class='content']/aside[@role='complementary']/section[2]/div[1]")
+  
+  set_age = sapply(details,function(feature) {feature %>%
+      html_nodes(xpath ="./dl/dt[text() = 'Age range']//following-sibling::dd[1]") %>%
+      html_text() %>%
+      str_trim() %>% as.character()
+  }) %>% sapply(function (feature) ifelse(length(feature) == 0, NA, feature))
+  
+  return(set_age)
+}
+
+setNumber = sapply(setLinks, FUN = getSetNumber)
+themeGroup = sapply(setLinks, FUN = getThemeGroup)
+theme = sapply(setLinks, FUN = getTheme)
+subtheme = sapply(setLinks, FUN = getSubtheme)
+year = sapply(setLinks, FUN = getYear)
+age = sapply(setLinks, FUN = getAge)
+
+test = data.frame(setNumber, setName, age, rating, themeGroup, theme, subtheme, pieces, minifigs, RRP, PPP, year, packaging, availability, instructions, setType, stringsAsFactors = FALSE)
 head(test)
